027a46a2fc8d71297db1f046a84cdaaedaafc2c3
BaptisteThomann <67266070+BaptisteThomann@users.noreply.github.com>
2020-07-06 23:04:38 +0200

    DAY 09

--- EX-01/admin.php
+++ EX-01/admin.php
@@ -1,23 +1,48 @@
 <html>
-<head>
-<title>mini-site-routing</title>
-</head>
-
 <body>
-<h1>
 <?php
+ 
 session_start();
+if($_SESSION['iamge']['size']>2097152){
+ $error="Le fichier est trop lourd";
+ echo $_SESSION['image']['size'];
+ echo "octets";
+};
+$findme='.';
+$position=strpos($_SESSION['image']['name'],$findme);
+echo $position;
+$longueur=strlen($_SESSION['image']['name'])-$position;
+$extension=substr($_SESSION['image']['name'],0,$longueur);
+$tailleextensio=strlen($extension);
+if($extension!='.jpg' ||'.jpeg'||'.png'){
+$error="Lextension de votre fichier n'est pas acceptée.";
+}
+ 
+$nom=strlen($_SESSION['image']['name']);
+$taillenom = $nom-$tailleextension;
+if($taillenom <=4){
+ echo 'Nombre de caractères ; '.$taillenom.'';
+ echo "Le nom de l'image doit faire minimum 4 carcatères hors extension.";
+}
+ 
 ?>
-</h1>
-    <form enctype="multipart/form-data" action="fileupload.php" method="post">
-      <input type="hidden" name="MAX_FILE_SIZE" value="2097152" />
-    <input type="file" name="monfichier" />
-    <input type="submit" />
-    <?php
-    $extensionsAutorisees = array("jpeg", "jpg", "png");
-    
-    ?>
-      
-    </form>
-  </body>
+<div>
+ <p>
+ <?php if(isset($error)) echo $error; ?>
+ </p>
+</div>
+ <form method="post" enctype="multipart/form-data">
+ <input type="file" name="image">
+ <input type="texte" name="titre" placeholder="Titre de la photo">
+ <input type="texte" name="descrpition" placeholder="Description de la photo">
+ <input type="submit" name="bouton" value="Charger">
+ </form>
+<?php
+echo $_SESSION['image'];
+echo $_SESSION['titre'];
+echo $_SESSION['description'];
+ 
+?>
+</body>
+ 
 </html>
